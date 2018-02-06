bc8081b89e97aedc2b8448c3f2bf3f2e3b137ccb
RenchiLiu <raylrc@gmail.com>
2018-02-06 12:09:53 -0600

    Initial Commit

---
+++ BookExplore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookExplore
+//
+//  Created by liurenchi on 2/6/18.
+//  Copyright © 2018 lrc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
